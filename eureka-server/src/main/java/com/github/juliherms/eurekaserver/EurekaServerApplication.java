c221fa62aa08ac8a3e9819917901d2e55dcd7add
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2021-04-25 18:57:25 -0300

    create eureka-server

--- eureka-server/src/main/java/com/github/juliherms/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/github/juliherms/eurekaserver/EurekaServerApplication.java
@@ -2,7 +2,13 @@ package com.github.juliherms.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * This projects responsible to implements service discovery/registry by scale another services
+ * in this case...Price-Service
+ */
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
