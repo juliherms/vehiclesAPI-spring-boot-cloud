688b7b5c097286f6f0005d0f0e40a1aee00fc36c
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2021-04-25 15:25:08 -0300

    implement test for resource price

--- pricing-service/src/test/java/com/github/juliherms/pricingservice/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/github/juliherms/pricingservice/PricingServiceApplicationTests.java
@@ -1,13 +1,62 @@
 package com.github.juliherms.pricingservice;
 
+import com.github.juliherms.pricingservice.model.Price;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import java.math.BigDecimal;
+import org.springframework.http.MediaType;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-class PricingServiceApplicationTests {
+
+/**
+ * This class responsible to test pricing endpoints
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class PricingServiceApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private MockMvc mvc;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
+	/**
+	 * Method responsible to test prices endpoint
+	 * Expected http status ok
+	 * @throws Exception
+	 */
+	@Test
+	public void testListPricesSuccess() throws Exception {
+
+		//call prices endpoint
+		mvc.perform(get("/prices/")
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.accept(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(status().isOk());
+
+	}
+
+	@Test
+	public void testGetPriceSuccess() throws Exception {
+
+		mvc.perform(get("/prices/1")
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.accept(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(status().isOk());
+	}
 }
