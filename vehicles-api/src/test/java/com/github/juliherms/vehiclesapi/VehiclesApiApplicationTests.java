2f1e0133819ac5fcf763f3d1ba29c59ba0954165
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2021-04-25 20:16:35 -0300

    create vehicles api

---
+++ vehicles-api/src/test/java/com/github/juliherms/vehiclesapi/VehiclesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.juliherms.vehiclesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
