38ad6f9d986bca3467df65a77b5a3bcb5e8f4aed
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2021-05-02 09:59:04 -0300

    fix: update car - condition

--- vehicles-api/src/main/java/com/github/juliherms/vehiclesapi/service/CarService.java
+++ vehicles-api/src/main/java/com/github/juliherms/vehiclesapi/service/CarService.java
@@ -82,6 +82,7 @@ public class CarService {
                     .map(carToBeUpdated -> {
                         carToBeUpdated.setDetails(car.getDetails());
                         carToBeUpdated.setLocation(car.getLocation());
+                        carToBeUpdated.setCondition(car.getCondition()); //fix for last code review
                         return repository.save(carToBeUpdated);
                     }).orElseThrow(CarNotFoundException::new);
         }
